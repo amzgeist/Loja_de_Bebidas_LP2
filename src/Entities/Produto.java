8b7e7c43c6349b0540576866acc1e33e13026b49
Carlos Augusto Ferreira Freire <carlos.augff@gmail.com>
2024-04-27 15:55:02 -0300

    criação da classe Produto

---
+++ src/Entities/Produto.java
@@ -0,0 +1,36 @@
+package Entities;
+
+public class Produto {
+    private static int proximoCodigo = 1;
+    private int codigo;
+    private String nome;
+    private float preco;
+    private int estoque;
+
+    public Produto(String nome, float preco, int estoque) {
+        this.codigo = proximoCodigo++;
+        this.nome = nome;
+        this.preco = preco;
+        this.estoque = estoque;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public float getPreco() {
+        return preco;
+    }
+
+    public int getEstoque() {
+        return estoque;
+    }
+
+    public void setEstoque(int estoque) {
+        this.estoque = estoque;
+    }
+}
